fe8c2639a713291844b25abcb49cb71b4713bb19
Evans Elabo <ellaboevans@gmail.com>
2026-03-27 08:24:13 +0000

    add last backup timestamp and auto-backup debounce constants

--- src/constants/extension.ts
+++ src/constants/extension.ts
@@ -1,2 +1,4 @@
 export const PAT_SECRET_KEY = "ark.pat";
 export const GIST_ID_KEY = "ark.gistId";
+export const LAST_BACKUP_KEY = "ark.lastBackupTimestamp";
+export const AUTO_BACKUP_DEBOUNCE_MS = 30000;
